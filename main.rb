fa7ca3827022a431892c90c6c350b1a23f27dd5c
bobcao3 <bobcaocheng@163.com>
2015-08-07 11:19:27 +0000

    Now support config file

--- main.rb
+++ main.rb
@@ -10,14 +10,21 @@ class MainWindow < Gtk::Window
 			Gtk.main_quit
 		}
 		
-		$vte = Vte::Terminal.new()
-		$vte.visible_bell = false
-		$vte.set_scrollback_lines(-1)
+		$vte = Terminal.new()
 		$pty = Vte::Pty.new(:default)
 		$vte.set_pty_object($pty)
 		self.add($vte)
 	end
 
+end
+
+class Terminal < Vte::Terminal
+
+	def initialize
+		super
+		getconfig
+	end
+	
 	def getconfig
 		# Default initialize
 		self.allow_bold = true
@@ -26,14 +33,13 @@ class MainWindow < Gtk::Window
 		# self.background-image-pixbuf = nil
 		self.background_opacity = 0
 		# self.background-saturation = 0
-		self.background_tint_color = Gdk::Color::Back
+		# self.background_tint_color = <GdkColor>
 		self.background_transparent = false
 		# self.cursor-blink-mode = <VteTerminalCursorBlinkMode>
 		# self.cursor-shape = <VteTerminalCursorShape>
 		# self.delete-binding = <VteTerminalEraseBinding>
 		self.encoding = "UTF-8"
 		# self.font-desc = <PangoFontDescription>
-		self.icon_title = "Terminal"
 		self.pointer_autohide = true
 		self.scroll_background = true
 		self.scroll_on_keystroke = true
@@ -41,23 +47,48 @@ class MainWindow < Gtk::Window
 		self.scrollback_lines = 255
 		self.visible_bell = false
 		# Read config
-		begin
-			config_file = File.open(Glib.getenv("HOME")+"/.config/ATerminal.config")
-			while line = config_file.gets.split("=")
-				#case line[0]
-				#when ""
-				#when ""
+		#begin
+			config_file = File.open(GLib.getenv("HOME")+"/.config/ATerminal.config")
+			while l = config_file.gets
+				line = l.split("=")
+				case line[0]
+				when "allow_bold"
+					self.allow_bold = (line[1] == "true")
+				when "audible_bell"
+					self.audible_bell = (line[1] == "true")
+				when "background_opacity" 
+					self.background_opacity = line[1].to_i
+				when "background_tint_color"
+					# self.background_tint_color = Gdk::Color::Back
+				when "background_transparent"
+					self.background_transparent = (line[1] == "true")
+				when "encoding"
+					self.encoding = line[1].split("\n")[0]
+				when "pointer_autohide"
+					self.pointer_autohide = (line[1] == "true")
+				when "scroll_background"
+					self.scroll_background = (line[1] == "true")
+				when "scroll_on_keystroke"
+					self.scroll_on_keystroke = (line[1] == "true")
+				when "scroll_on_output"
+					self.scroll_on_output = (line[1] == "true")
+				when "scrollback_lines"
+					self.scrollback_lines = line[1].to_i
+				when "visible_bell"
+					self.visible_bell = (line[1] == "true")
+				end
 			end
-		rescue Exception
-			false
-		end
+			config_file.close
+		#rescue Exception
+		#	false
+		#end
 		
 	end
 
 end
 	
 Gtk.init
-a = MainWindow.new
+a =MainWindow.new
 a.show_all
 $vte.fork_command(GLib.getenv("SHELL"))
 $vte.grab_focus
